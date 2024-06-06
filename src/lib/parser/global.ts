33be365ec05f49f46cbfdb6db8eb6df15305becb
Simone Del Popolo <simonedelpopolo@outlook.com>
2024-06-06 10:55:10 +0000

    Documenting and parameter check
    - adding doc comments
    - checking the definition parameter to be full given
    - it throws

--- src/lib/parser/global.ts
+++ src/lib/parser/global.ts
@@ -11,22 +11,41 @@ global_flag_declaration.set( 'has_global', false );
  *       requires reading the command_specification
  *       requires reading the global_flag_specification
  *       requires reading the flag_specification
+ *       doing this will deprecate the set_global_flag_declaration function
  */
 
-// function that set the global flag specification
 /**
  * Sets the global flag declaration.
  * The global flag declaration tells the parser that the cli has global flags.
+ * the parser will process the global flags before the command and its flags.
  *
- * global_index is an array of indexes that tells the parser on how many (approximately) indexes to skip/check
- * before finding the command in the process.argv.
- *
- * command_identifier_list is an array of strings that tells the parser what the command identifiers are.
- * global_identifier_list is an array of strings that tells the parser what the global identifiers are.
- *
- * iterates over the declaration Map and sets the global flag declaration.
+ * @param declaration - The global flag declaration.
+ * The global flag declaration is a map that contains the following:
+ * - has_global is a boolean that tells the parser that the cli has global flags.
+ * - command_identifier_list is an array of strings that tells the parser what the command identifiers are.
+ * - global_identifier_list is an array of strings that tells the parser what the global identifiers are.
+ * @throws {Error} - If the declaration.has_global isn't set.
+ * @throws {Error} - If the declaration.has_global set to false.
+ * @throws {Error} - If the declaration.command_identifier_list isn't set.
+ * @throws {Error} - If the declaration.global_identifier_list isn't set.
  */
 export function set_global_flag_declaration( declaration: GlobalFlagDeclaration ): void {
+
+  if ( ! declaration.has( 'has_global' ) ) {
+    throw( 'missing has_global. if the CLI has global flags it must have a has_global boolean set to true.' );
+  }
+
+  if ( declaration.has( 'has_global' ) && declaration.get( 'has_global' ) === false ) {
+    throw( 'unnecessary call to this function. if the CLI has global flags, set the has_global to true' );
+  }
+
+  if( ! declaration.has( 'cli_command_identifier_list' ) ) {
+    throw( 'missing cli_command_identifier_list. if the CLI has global flags it must have a command_identifier_list containing at least one command, two commands in the list makes more sense btw.' );
+  }
+
+  if( ! declaration.has( 'cli_global_identifier_list' ) ) {
+    throw( 'missing cli_global_identifier_list. if the CLI has global flags it must have a global_identifier_list containing at least one global flag.' );
+  }
 
   for( const [ key, value ] of declaration.entries() ) {
 
